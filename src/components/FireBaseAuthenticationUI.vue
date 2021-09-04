e3040e87432b8b364b3433e34a8f50cfe420ddd0
nicolas <n.damours.00@gmail.com>
2021-09-04 15:05:17 -0400

    [FEATURE]
    WIP: worked on adding support for firebase authentication when on device.

--- src/components/FireBaseAuthenticationUI.vue
+++ src/components/FireBaseAuthenticationUI.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="firebase-authentication-ui"></div>
+  <div id="firebase-authentication-ui" v-if="!isMobile"></div>
+  <div v-if="isMobile">
+    <ion-button @click="handleSignInWithGoogleClick">
+      {{ t('authentication.signInWithGoogle') }}
+      <ion-icon :icon="google" />
+    </ion-button>
+  </div>
 </template>
 
 <script>
@@ -7,31 +13,59 @@ import * as firebaseUI from 'firebaseui';
 import getAuthInstance from "@/auth"
 import {computed, onMounted} from "vue";
 import {GoogleAuthProvider, EmailAuthProvider} from "firebase/auth";
+import {IonIcon, isPlatform} from "@ionic/vue";
+import { FirebaseAuthentication } from "@ionic-native/firebase-authentication";
+import {useI18n} from "vue-i18n";
+import { logoGoogle } from "ionicons/icons"
 
 export default {
   name: "FireBaseAuthenticationUI",
   emits: ["authentication-success"],
+  components: {
+    IonIcon
+  },
   setup(_, {emit}) {
-
+    const { t } = useI18n();
     const UI = computed(() => {
       const instance = firebaseUI.auth.AuthUI.getInstance();
       return instance || new firebaseUI.auth.AuthUI(getAuthInstance());
     });
 
+    const isMobile = computed(() => isPlatform('ios') || isPlatform('android'))
+
     onMounted(() => {
-      UI.value.start('#firebase-authentication-ui', {
-        callbacks: {
-          signInSuccessWithAuthResult: () => {
-            emit("authentication-success")
-            return false;
-          }
-        },
-        signInOptions: [
-          GoogleAuthProvider.PROVIDER_ID,
-          EmailAuthProvider.PROVIDER_ID,
-        ]
-      });
-    })
+      if(!isMobile.value) {
+        UI.value.start('#firebase-authentication-ui', {
+          callbacks: {
+            signInSuccessWithAuthResult: () => {
+              emit("authentication-success")
+              return false;
+            }
+          },
+          signInOptions: [
+            GoogleAuthProvider.PROVIDER_ID,
+            EmailAuthProvider.PROVIDER_ID,
+          ]
+        });
+      }
+    });
+
+
+    const handleSignInWithGoogleClick = async () => {
+      console.log('before signIn')
+      const results = FirebaseAuthentication.signInWithGoogle(process.env.VUE_APP_FIRESTORE_PROJECT_ID, process.env.VUE_APP_FIRESTORE_API_KEY);
+      console.log('results', results);
+      if(results) {
+        emit("authentication-success")
+      }
+    }
+
+    return {
+      t,
+      isMobile,
+      logoGoogle,
+      handleSignInWithGoogleClick
+    }
   }
 }
 </script>
--- src/composable/use-bind-authentication.ts
+++ src/composable/use-bind-authentication.ts
@@ -1,20 +1,39 @@
 import getAuthInstance from "@/auth"
-import { onAuthStateChanged } from "firebase/auth";
+import {onAuthStateChanged} from "firebase/auth";
 import useAuthentication from "@/composable/use-authentication";
 import firebase from "firebase/compat";
 import User = firebase.User;
+import {isPlatform} from "@ionic/vue";
+import {FirebaseAuthentication} from "@ionic-native/firebase-authentication";
 
 export default function useBindAuthentication(): Promise<User> {
-    const { showAuthenticationModal } = useAuthentication();
+    const {showAuthenticationModal} = useAuthentication();
     const authenticationInstance = getAuthInstance();
-    return new Promise(resolve => {
-        onAuthStateChanged(authenticationInstance, async user => {
-            if(user === null) {
-                await showAuthenticationModal()
-            }
 
-            user = authenticationInstance.currentUser
-            resolve(user as User);
-        });
+    const authenticationCallback = (resolve: Function) => async (user: any) => {
+        console.log('in callback')
+        if (user === null) {
+            await showAuthenticationModal()
+        }
+
+        user = authenticationInstance.currentUser
+        resolve(user as User);
+    };
+
+
+    console.log('before promise');
+    return new Promise(resolve => {
+        if (isPlatform('ios') || isPlatform('android')) {
+            FirebaseAuthentication.getCurrentUser().then((user) => {
+                console.log('user', user);
+                return authenticationCallback(resolve)
+            });
+            FirebaseAuthentication.onAuthStateChanged().toPromise().then(authenticationCallback(resolve));
+
+            return showAuthenticationModal();
+
+        } else {
+            onAuthStateChanged(authenticationInstance, authenticationCallback(resolve));
+        }
     })
 }

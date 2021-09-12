3c7f5eb4a37b4673f6bd65661d58654392d0cfe6
nicolas <n.damours.00@gmail.com>
2021-09-12 12:42:12 -0400

    [FEATURE]
    Added config to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,17 @@ module.exports = {
       runtimeOnly: false,
       compositionOnly: false,
       fullInstall: true
+    },
+    pwa: {
+      themeColor: "#2dd36f",
+      msTileColor: "#ffffff",
+      iconPaths: {
+        favicon32: 'assets/icon/favicon-32x32.png',
+        favicon16: 'assets/icon/favicon-16x16.png',
+        appleTouchIcon: 'assets/icon/apple-touch-icon-152x152.png',
+        maskIcon: 'assets/icon/safari-pinned-tab.svg',
+        msTileImage: 'assets/icon/msapplication-icon-144x144.png'
+      }
     }
   }
 }

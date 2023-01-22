6e629adc40f0bd69b04c66239fcbcdead9d4336c
nicolas <n.damours.00@gmail.com>
2023-01-22 16:26:56 -0500

    [FEATURE]
    Fixed formatting for deployment

--- functions/tests/unit/Functions/share-week-with-email.spec.ts
+++
@@ -1,9 +0,0 @@
-describe("share-week-with-email", () => {
-    it("should call the single week sharing when sharing only one week", () => {
-        // given a payload with a shareAllWeeks attribute as false
-        const payload = {
-            email: "irrelevent",
-            shareAllWeeks: false,
-        };
-    })
-})

5b1af0a1c2ab28a23014917f3ff710465690fd5f
nicolas <n.damours.00@gmail.com>
2023-10-03 21:39:18 -0400

    [FEATURE]
    WIP: working on linking week sharing and week in a sub collection of week.

--- functions/src/Hooks/update-week-sharing-on-week-update.ts
+++ functions/src/Hooks/update-week-sharing-on-week-update.ts
@@ -1,7 +1,17 @@
-import * as functions from "firebase-functions";
 import {getApp} from "../app-instance";
+import {DocumentSnapshot} from "firebase-functions/lib/v1/providers/firestore";
+import {Change} from "firebase-functions/lib/common/change";
 
-exports.updateWeekSharingOnWeekUpdate = functions.database.ref("/weeks").onCreate(async (snapshot) => {
-    const original = snapshot.val();
+export default async function updateWeekSharingOnWeekUpdate(snapshot: Change<DocumentSnapshot>) {
     const app = getApp();
-});
+
+    const weekAuthor = snapshot.before.get("user");
+
+    const weekSharingForAuthor = await app.firestore().collection("/weekSharing").where("authorId", "==", weekAuthor).get();
+
+    weekSharingForAuthor.forEach(async (weekSharing) => {
+        await app.firestore().collection(`/weeks/${snapshot.before.id}/sharedWith`).add({
+            userId: weekSharing.get("targetId"),
+        });
+    });
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,9 @@ import shareWithEmail from "./Functions/share-with-email";
 import dependencies from "./Functions/lib/dependencies";
 import acceptShareRequest from "./Functions/accept-share-request";
 import unShareWithEmail from "./Functions/unshare-with-email";
+import updateWeekSharingOnWeekUpdate from "./Hooks/update-week-sharing-on-week-update";
+
+import * as functions from "firebase-functions";
 
 dependencies();
 
@@ -12,3 +15,7 @@ exports.shareWithEmail = shareWithEmail;
 exports.unshareWithEmail = unShareWithEmail;
 exports.acceptShareRequest = acceptShareRequest;
 exports.removeCurrentUserFromShare = removeCurrentUserFromShare;
+exports.removeCurrentUserFromShare = removeCurrentUserFromShare;
+
+
+exports.updateWeekSharingOnWeekUpdate = functions.firestore.document("/weeks/{weekId}").onWrite(updateWeekSharingOnWeekUpdate);

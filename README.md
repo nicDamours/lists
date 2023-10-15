b3a5366fbd1a08d3b62257dafea771571545d953
nicDamours <n.damours.00@gmail.com>
2023-10-15 14:57:29 -0400

    Feature/add about modal (#12)
    * [FEATURE]
    
    Added readme and licence.
    added about page.

---
+++ README.md
@@ -0,0 +1,27 @@
+# MaListe
+
+MaListe ( french for "my list" ) is a simple list keeping application, that allows you to keep track of multiple lists
+and share them with other users.
+
+## Features
+
+### List keeping
+
+- Allow you to keep multiple lists
+- Lists are split into sections, which can be used to group items together.
+- Lists can be shared with another user, which allow them to view and edit the list.
+
+### Week planning
+
+- Allow you to plan your week in an easy-to-use table.
+- Week can also be shared with other users.
+- Sharing a week with another user allow them to view and update your week. This is useful when two user has the same
+  meal plan.
+- The week days are split into three categories:
+  - Lunch,
+  - Diner
+  - Activities for the day.
+
+## Contributing
+
+Feel free to fork the project, create issues and PR if you see fit.
---
+++ src/components/modal/AboutModal.vue
@@ -0,0 +1,67 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>{{ t('modals.about.title') }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <div class="o-about__container">
+        <div class="o-about__header">
+          <ion-icon :icon="cart" class="o-about__icon" size="large"></ion-icon>
+          <ion-text>
+            <h3>Ma Liste</h3>
+          </ion-text>
+        </div>
+        <div class="o-about__content">
+          <ion-text>
+            <p>{{ t('modals.about.author') }} <a href="https://github.com/nicDamours">nicDamours</a> ❤️</p>
+          </ion-text>
+
+          <ion-text>
+            <p>{{ t('modals.about.softwareCost') }}</p>
+          </ion-text>
+
+          <ion-text>
+            <p>{{ t('modals.about.foundABug') }} <a
+                href="https://github.com/nicDamours/lists/issues/new/choose">{{ t('modals.about.reportIssue') }}</a> 🪲
+            </p>
+          </ion-text>
+        </div>
+
+
+        <div class="o-about__footer">
+          <a href="https://www.buymeacoffee.com/nicDamours" target="_blank"><img
+              alt="Buy Me A Coffee" src="https://cdn.buymeacoffee.com/buttons/v2/default-green.png"
+              style="height: 60px !important;width: 217px !important;"></a>
+          <a href="https://github.com/nicDamours/lists" target="_blank">
+            <ion-icon :icon="logoGithub" class="o-about__github"></ion-icon>
+          </a>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {useI18n} from "vue-i18n";
+import {cart, logoGithub} from 'ionicons/icons';
+import {IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar} from "@ionic/vue";
+
+export default {
+  name: "AboutModal",
+  components: {
+    IonIcon, IonContent, IonHeader, IonToolbar, IonTitle,
+    IonPage
+  },
+  setup() {
+    const {t} = useI18n();
+
+    return {
+      t,
+      cart,
+      logoGithub
+    }
+  }
+}
+</script>
